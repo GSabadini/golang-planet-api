683899277a49921151efb86030bb58c08d599d39
Gabriel S Facina <gabriel.facina@pismo.io>
2021-05-17 23:02:09 -0300

    added use case of adding a new planet

--- domain/planet.go
+++ domain/planet.go
@@ -1,15 +1,11 @@
 package domain
 
-import "context"
+import (
+	"context"
+	"time"
+)
 
 type (
-	Planet struct {
-		id      string
-		name    string
-		climate string
-		ground  string
-	}
-
 	// PlanetCreator defines the operation of creating a account entity
 	PlanetCreator interface {
 		Create(context.Context, Planet) (Planet, error)
@@ -26,4 +22,42 @@ type (
 	PlanetDeleter interface {
 		Delete(context.Context, int64) error
 	}
+
+	Planet struct {
+		id        string
+		name      string
+		climate   string
+		ground    string
+		createdAt time.Time
+	}
 )
+
+func NewPlanet(id string, name string, climate string, ground string, time time.Time) Planet {
+	return Planet{
+		id:        id,
+		name:      name,
+		climate:   climate,
+		ground:    ground,
+		createdAt: time,
+	}
+}
+
+func (p Planet) ID() string {
+	return p.id
+}
+
+func (p Planet) Name() string {
+	return p.name
+}
+
+func (p Planet) Climate() string {
+	return p.climate
+}
+
+func (p Planet) Ground() string {
+	return p.ground
+}
+
+func (p Planet) CreatedAt() time.Time {
+	return p.createdAt
+}
--- usecase/create_planet.go
+++ usecase/create_planet.go
@@ -17,7 +17,7 @@ type (
 	CreatePlanetInput struct {
 		Name    string `json:"name" validate:"required"`
 		Climate string `json:"climate" validate:"required"`
-		Ground  int64  `json:"ground" validate:"required"`
+		Ground  string `json:"ground" validate:"required"`
 	}
 
 	// CreatePlanetPresenter output port
@@ -27,9 +27,11 @@ type (
 
 	// CreatePlanetOutput output data
 	CreatePlanetOutput struct {
-		ID      string `json:"id"`
-		Climate string `json:"climate"`
-		Ground  int64  `json:"ground"`
+		ID        string `json:"id"`
+		Name      string `json:"name"`
+		Climate   string `json:"climate"`
+		Ground    string `json:"ground"`
+		CreatedAt string `json:"created_at"`
 	}
 
 	createPlanetInteractor struct {
@@ -38,3 +40,29 @@ type (
 		ctxTimeout time.Duration
 	}
 )
+
+func NewCreatePlanetInteractor(repo domain.PlanetCreator, presenter CreatePlanetPresenter, ctxTimeout time.Duration) CreatePlanetUseCase {
+	return createPlanetInteractor{
+		repo:       repo,
+		presenter:  presenter,
+		ctxTimeout: ctxTimeout,
+	}
+}
+
+func (c createPlanetInteractor) Execute(ctx context.Context, input CreatePlanetInput) (CreatePlanetOutput, error) {
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	planet, err := c.repo.Create(ctx, domain.NewPlanet(
+		"uuid.New()",
+		input.Name,
+		input.Climate,
+		input.Ground,
+		time.Now(),
+	))
+	if err != nil {
+		return c.presenter.Output(domain.Planet{}), err
+	}
+
+	return c.presenter.Output(planet), nil
+}
